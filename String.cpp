89bb8af6a311dba57e9fd3b8a4781601ace3b5ca
出久 <118390636+asim1909@users.noreply.github.com>
2023-08-03 09:36:24 +0530

    Create String.cpp

---
+++ String.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int roll_no;
+    cout<<"entre the name and rollno\n";
+    cin>>name;
+    cin>>roll_no;
+    cout<<name<<" "<<roll_no;
+}
