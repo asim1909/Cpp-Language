e5deb2aa751101d345fda77347b9397382d41d58
出久 <118390636+asim1909@users.noreply.github.com>
2023-08-04 21:27:01 +0530

    Create Km_to_m.cpp

---
+++ Km_to_m.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float km, m;
+    cout << "Enter distance in km: ";
+    cin>>km;
+    m = km*1000;
+    cout<<km<< "in km = "<<m<< " in m";
+    return 0;
+}
