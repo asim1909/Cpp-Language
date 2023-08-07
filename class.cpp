15c00841e9eb748376b6562560d239afd5d1742a
出久 <118390636+asim1909@users.noreply.github.com>
2023-08-07 10:31:30 +0530

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,30 @@
+// area of rectangle using class
+
+// 
+
+#include<iostream>  
+using namespace std;
+
+class rectangle{
+    private:
+        int l,b;
+    public:
+        void get()
+        {
+            cout<<"enter the 1 and b\n";
+            cin>>l>>b;
+        }
+        void area()
+        {
+            cout<<"area is "<<l*b;
+        }
+};
+int main()
+{
+    rectangle r1,r2;
+    r1.get();
+    r1.area();
+    r2.get();
+    r2.area();
+    return 0;
+}
